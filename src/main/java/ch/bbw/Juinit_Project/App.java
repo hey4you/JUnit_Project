576df3d58d4df8c5f0938a65a00e1c42f3219dbd
5ia16niwyss <5ia16niwyss@gibwintra.ch>
2018-12-14 08:39:52 +0100

    set up calculater Class and App impl.

--- src/main/java/ch/bbw/Juinit_Project/App.java
+++ src/main/java/ch/bbw/Juinit_Project/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Console Calculator" );
+        System.out.println( "===================");
+        System.out.println();
+        
+        Calculator calculator = new Calculator();
+        int valueA = 0;
+        int valueB = 0;
+        
+        valueA = 10;
+        valueB = 20;
+        System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(10, 20));
+        
+        
     }
 }
