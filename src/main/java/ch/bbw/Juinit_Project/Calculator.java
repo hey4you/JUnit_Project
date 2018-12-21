923fea2c958ca15790faf6b9ca9c598d7cc417cc
hey4you <nils@ardevia.ch>
2018-12-21 08:05:12 +0100

    changed package visibility in Class Calculator

--- src/main/java/ch/bbw/Juinit_Project/Calculator.java
+++ src/main/java/ch/bbw/Juinit_Project/Calculator.java
@@ -5,13 +5,13 @@ public class Calculator {
 	public int summe(int summand1, int summand2){
 		return summand1 + summand2;
 	}
-	public int diff(int diff1, int diff2){
+	protected int diff(int diff1, int diff2){
 		return diff1 - diff2;
 	}
 	public int product(int product1, int product2){
 		return product1 * product2;
 	}
-	public int quets(int quets1, int quets2){
+	int quets(int quets1, int quets2){
 		return quets1 / quets2;
 	}
 	public void exceptionDivisonZero(){
